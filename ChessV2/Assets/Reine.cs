2a7873cb2b3a4aceeb42553c4289ab71074e3316
baya209 <6235852@bdeb.qc.ca>
2025-03-28 16:20:52 -0400

    creation méthodes

--- ChessV2/Assets/Reine.cs
+++ ChessV2/Assets/Reine.cs
@@ -1,7 +1,19 @@
 ﻿using System;
 public class Reine : Piece
 {
-    public Reine()
+    public Reine(int[,] tableau, int ligne, int colonne, int couleur) : base(tableau, ligne, colonne, couleur)
     {
+        setSymbole('re');
     }
+
+    public override bool deplacer(int l, int c)
+    {
+        return true
+    }
+
+    public override bool[,] isDanger(bool[,] danger)
+    {
+        return true
+    }
+
 }
