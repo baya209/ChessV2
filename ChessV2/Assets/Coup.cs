6e1eb3feca25ad2da7ff724ca346900d982291ee
bababoi880 <aliacayer@gmail.com>
2025-03-30 19:55:55 -0400

    modification classe Coup

--- ChessV2/Assets/Coup.cs
+++ ChessV2/Assets/Coup.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class  Coup
 {
     public int li, ci, lf, cf;
     public int pieceCapturee;// valeur piece capturee (-1 si il n'y a pas de piece)
 
-    public NewBehaviourScript(int li, int ci, int lf, int cf, int pieceCapturee = -1)
+    public Coup(int li, int ci, int lf, int cf, int pieceCapturee = -1)
     {
         this.li = li;
         this.ci = ci;
