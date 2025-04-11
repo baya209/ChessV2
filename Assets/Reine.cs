1ee86d2896040bc9f6215f121b40b33553ca502b
baya209 <6235852@bdeb.qc.ca>
2025-04-11 15:42:32 -0400

    .

--- Assets/Reine.cs
+++ Assets/Reine.cs
@@ -108,8 +108,9 @@ public class Reine : Piece
             }
             else
             {
-                Debug.LogError("Trajectoire non valide pour la reine");  un déplacement de reine
+                Debug.LogError("Trajectoire non valide pour la reine");
             }
+
         }
         else
         {
