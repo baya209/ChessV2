911a10097b447c73fe838eb9b230446ab92c600b
Che1ton <richbaz12@gmail.com>
2025-05-13 02:41:26 -0400

    essaieServeur
    Création d'un serveur avec la possibilité qu'un joueur puisse rejoindre (du moment ou le joueur hôte clique sur le bouton jouer)

---
+++ Assets/ClientConnector.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ClientConnector : MonoBehaviour
+{
+    private TcpClient client;
+
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        try
+        {
+            client = new TcpClient("127.0.0.1", 8080);
+            Debug.Log(" Connected to server!");
+            
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(" Failed to connect to server: " + ex.Message);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (client != null && client.Connected)
+        {
+            client.Close();
+            Debug.Log(" Disconnected from server.");
+        }
+    }
+}
---
+++ Assets/ServerManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//Pour le serveur LAN 
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class ServerManager : MonoBehaviour
+{
+    TcpListener serveur = null;
+
+    /** Proposition de CHATGPT pour régler le problème de freeze en créant un deuxième Threand
+        * pour pouvoir gérer la logique du serveur en parallèle. **/
+     Thread serveurThread;
+
+    public static void Main()
+    {
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Proposition pour lancer le nouveau Threand dans l'arrière plan 
+        serveurThread = new Thread(lancerServeur);
+        serveurThread.IsBackground = true;
+        serveurThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void lancerServeur()
+    {
+        try
+        {
+            //Instancier le port d'entrée du serveur 
+            // Int32 --> 32 bits 
+            Int32 port = 8080;
+            // Permet de se connecter directeemnt à l'adresse IP sur lequel se trouve le pc hôte
+            IPAddress adresseLocale = IPAddress.Parse("127.0.0.1");
+
+            // Instancier le server en indiquant le port d'entrée et l'adresse IP de la connexion
+            serveur = new TcpListener(adresseLocale, port);
+
+            serveur.Start();
+
+            if (serveurThread != null)
+            {
+                Debug.Log("Attente connexion...");
+            }
+
+            TcpClient client1 = serveur.AcceptTcpClient();
+
+            Debug.Log("Connected!");
+
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine(e.ToString());
+            Debug.Log("Erreur de serveur");
+            serveur.Stop();
+
+        }
+        finally
+        {
+            
+            
+        }
+
+    }
+}
