eb1c2dbed9b8829aa9073a6de298f2d5855bdee5
Che1ton <richbaz12@gmail.com>
2025-05-19 13:22:17 -0400

    Positionnement pieces
    Utiliser la logique de "Plateau" et "Partie" afin de positionner les pièces

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,10 @@ public class GameManager : MonoBehaviour
     public Text echiquier;
     Partie partie = new Partie();
 
-    public InputField fli;
+    [SerializeField] private PlateuDeJeu plateuDeJeu;
+    public GameObject[] piecePrefabs;
+
+    //public InputField fli;
     public string li;
     public string ci;
     public string lf;
@@ -19,15 +23,121 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         partie.creerPartie();
-        echiquier.text = partie.afficher();
+        // echiquier.text = partie.afficher(); <----------->
+        List<Piece> pieces = partie.getPlateau().getPieces();
+        List<GameObject> pieces3D = new List<GameObject>();
+
+        float posX = pieces[0].getColonne();
+        foreach (var piece in pieces)
+        {
+            GameObject piece3D = new GameObject();
+            
+            if (piece is Pion)
+            {
+               piece3D.layer = LayerMask.NameToLayer("Pion");
+               
+                Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+                if(piece.getCouleur() == 1)
+                    piece3D = Instantiate(piecePrefabs[0], pos, piecePrefabs[0].transform.rotation );
+                else
+                    piece3D = Instantiate(piecePrefabs[1], pos, piecePrefabs[1].transform.rotation);
+
+                pieces3D.Add(piece3D);
+                Debug.Log("Nouvelle Pièce");
+            }
+            
+               else if (piece is Roi)
+               {
+                    piece3D.layer = LayerMask.NameToLayer("Roi");
+                   
+                    Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+
+                    if (piece.getCouleur() == 1)
+                        piece3D = Instantiate(piecePrefabs[3], pos, piecePrefabs[3].transform.rotation);
+                    else
+                        piece3D = Instantiate(piecePrefabs[2], pos, piecePrefabs[2].transform.rotation);
+
+                    pieces3D.Add(piece3D);
+                    Debug.Log("Nouvelle Pièce");
+               }
+           
+            
+            
+            if (piece is Tour)
+            {
+                piece3D.layer = LayerMask.NameToLayer("Tour");
+
+                Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+                if (piece.getCouleur() == 1)
+                    piece3D = Instantiate(piecePrefabs[5], pos, piecePrefabs[5].transform.rotation);
+                else
+                    piece3D = Instantiate(piecePrefabs[4], pos, piecePrefabs[4].transform.rotation);
+
+                pieces3D.Add(piece3D);
+            }
+            
+            
+            if (piece is Reine)
+            {
+                piece3D.layer = LayerMask.NameToLayer("Reine");
+
+                Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+                if (piece.getCouleur() == 1)
+                    piece3D = Instantiate(piecePrefabs[6], pos, piecePrefabs[6].transform.rotation);
+                else
+                    piece3D = Instantiate(piecePrefabs[7], pos, piecePrefabs[7].transform.rotation);
+
+                pieces3D.Add(piece3D);
+            }
+            
+            
+            if (piece is Fou)
+            {
+                piece3D.layer = LayerMask.NameToLayer("Fou");
+
+                Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+                if (piece.getCouleur() == 1)
+                    piece3D = Instantiate(piecePrefabs[8], pos, piecePrefabs[8].transform.rotation);
+                else
+                    piece3D = Instantiate(piecePrefabs[9], pos, piecePrefabs[9].transform.rotation);
+
+                pieces3D.Add(piece3D);
+            }
+            
+            
+            if (piece is Cavalier)
+            {
+                piece3D.layer = LayerMask.NameToLayer("Cavalier");
+
+                Vector3 pos = plateuDeJeu.GetCentreTuile(piece.getColonne(), piece.getLigne());
+
+                if (piece.getCouleur() == 1)
+                    piece3D = Instantiate(piecePrefabs[10], pos, piecePrefabs[10].transform.rotation);
+                else
+                    piece3D = Instantiate(piecePrefabs[11], pos, piecePrefabs[11].transform.rotation);
+
+
+                pieces3D.Add(piece3D);
+            }
+
+        }
     }
   
     // Update is called once per frame
     void Update()
     {
-        user = inputField.text;
+       // user = inputField.text;
+        
 
+
+        /**
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             Debug.Log("enter");
@@ -57,6 +167,8 @@ public class GameManager : MonoBehaviour
             // Call your game logic
             partie.jouerCoup(diviser[0], diviser[1], diviser[2], diviser[3], diviser[4]);
             echiquier.text = partie.afficher();
+        
         }
+        **/
     }
 }
