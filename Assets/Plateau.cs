55aab5c0c5a3b5f90c72104c978a8591d8c9547a
Che1ton <richbaz12@gmail.com>
2025-05-25 13:19:50 -0400

    Nettoyage code

--- Assets/Plateau.cs
+++ Assets/Plateau.cs
@@ -9,21 +9,6 @@ public class Plateau
     
     private bool[,] dangerNoir = new bool[8,8];
     private bool[,] dangerBlanc = new bool[8, 8];
-    private int[,] tableauModulo = new int[8,8];
-    /*
-    2 = blanc
-    3 = noir
-    5 = vide
-    7 = zone danger noir
-    11 = zone danger blanc
-    13 = pion
-    17 = tour
-    19 = cavalier
-    23 = fou
-    29 = reine
-    31 = roi
-     */
-
 
     private int[,] tableau = new int[8, 8]; // -1 = noir /// 0 = vide /// 1= blanc
     List<Piece> pieces = new List<Piece>();
@@ -37,11 +22,6 @@ public class Plateau
     public void setTableau(in int[,] tableau)
     {
         this.tableau = tableau;
-    }
-    public int[,] getTableauModulo() { return tableauModulo; }
-    public void setTableauModulo( int[,] tableauModulo)
-    {
-        this.tableauModulo = tableauModulo;
     }
     public void setPieces(List<Piece> pieces) {  this.pieces = pieces; }
     public List<Piece> getPieces() { return pieces; }
@@ -57,7 +37,7 @@ public class Plateau
     public bool[,] getDangerBlanc() { return dangerBlanc; }
 
 
-    // ecrire deplacement speciaux dans jeu
+    // ecrire les deplacements speciaux dans jeu
     public List<Coup> GenererTousLesCoups(int couleur)
     {
         List<Coup> tousLesCoups = new List<Coup>();
@@ -125,20 +105,5 @@ public class Plateau
 
         return copie;
     }
-    /*
-    public int EvaluerPlateau(int couleur)
-    {
-        int score = 0;
-
-        foreach (Piece piece in pieces)
-        {
-            
-            // pour savoir le tour
-            score += piece.getValeur() * piece.getCouleur();
-        }
-
-        // Ajuster le score selon la couleur qui joue
-        return score * couleur;
-    }
-    */
+    
 }
--- Assets/Reine.cs
+++ Assets/Reine.cs
@@ -63,7 +63,8 @@ public class Reine : Piece
                     Debug.LogError(directionLigne);// -----
                     **/
 
-                    //Incremente la position à vérifier case par case (en effectuant la première vérification à partir de la deuxième case) si non ca break directement de la méthode
+                    //Incremente la position à vérifier case par case (en effectuant la première vérification à partir de la deuxième case)
+                    //si non ca break directement de la méthode, car la première case est occupé par la pièce elle-même
 
                     ligneActuelle += directionLigne; // si cest vert le haut ça aditionne si c'est vers le bas ça soustrait
                     colonneActuelle += directionColonne; // si cest vert le haut ça aditionne si c'est vers le bas ça soustrait
@@ -79,6 +80,8 @@ public class Reine : Piece
                 Debug.LogError("Deplacement invalide");
                 return false;
             }
+
+                                        //Déplacer comme la tour
             else if (getTableau()[l, c] == 0 || getTableau()[l, c] == (-1 * getCouleur()))//Verifie que la case est vide 
             {
                 if (c != getColonne() && l == getLigne())
@@ -153,136 +156,17 @@ public class Reine : Piece
 
             else
             {
-                Debug.LogError("Trajectoire non valide pour le Fou");
+                Debug.LogError("Trajectoire non valide pour la Reine");
                 return false;
             }
             return false;
         }
-
-
-
- 
-
-        /** 
-         * ------ Ancien code --------
-
-        if (getTableau()[l, c] == 0 || getTableau()[l, c] == (-1 * getCouleur()))
-        {
-            if (c != getColonne() && l == getLigne())
-            {
-                if (c > getColonne()) 
-                {
-                    for (int i = getColonne() + 1; i <= c; i++) 
-                    {
-                        if (getTableau()[i, l] != 0) 
-                        {
-                            return false;
-                        }
-                    }
-                    return true; 
-                }
-                else 
-                {
-                    for (int i = c - 1; i > getColonne(); i--) 
-                    {
-                        if (getTableau()[i, l] != 0) 
-                        {
-                            return false;
-                        }
-                    }
-                    return true; 
-                }
-            }
-
-            else if (l != getLigne() && c == getColonne())
-            {
-                if (l > getLigne()) // Déplacement vers le bas
-                {
-                    for (int i = getLigne() + 1; i <= l; i++) // Vérifie toutes les cases entre la position actuelle et la cible
-                    {
-                        if (getTableau()[c, i] != 0) // Si une pièce bloque le passage
-                        {
-                            return false;
-                        }
-                    }
-                    return true; // Le chemin est libre, déplacement possible
-                }
-                else // Déplacement vers le haut
-                {
-                    for (int i = l - 1; i > getLigne(); i--) // Vérifie toutes les cases entre la position actuelle et la cible
-                    {
-                        if (getTableau()[c, i] != 0) // Si une pièce bloque le passage
-                        {
-                            return false;
-                        }
-                    }
-                    return true; // Le chemin est libre, déplacement possible
-                }
-            }
-
-            // Vérifie un déplacement diagonal
-
-            else if (Math.Abs(l - getLigne()) == Math.Abs(c - getColonne()))
-            {
-
-                int directionLigne;
-                if (l > getLigne())
-                {
-                    directionLigne = 1; // Se déplace vers le bas
-                }
-                else
-                {
-                    directionLigne = -1; // Se déplace vers le haut
-                }
-                int directionColonne;
-
-                if (c > getColonne())
-                {
-                    directionColonne = 1; // droite
-
-                }
-                else
-                {
-                    directionColonne = -1; //gauche
-                }
-
-
-                int ligneActuelle = getLigne() + directionLigne; //initialiser la ligne actuelle 
-                int colonneActuelle = getColonne() + directionColonne; // Initialise la colonne actuelle 
-
-                while (ligneActuelle != l && colonneActuelle != c) // Parcourt les cases diagonales jusqu'à la position rentrée
-                {
-                    if (getTableau()[ligneActuelle, colonneActuelle] != 0) // Vérifie si une pièce bloque le passage
-                    {
-                        return false;
-                    }
-                    ligneActuelle += directionLigne; // si cest vert le haut ça aditionne si c'est vers le bas ça soustrait
-                    colonneActuelle += directionColonne; // si cest vert le haut ça aditionne si c'est vers le bas ça soustrait
-
-                }
-                return true; 
-            }
-            else
-            {
-                Debug.LogError("Trajectoire non valide pour la reine");  
-            }
-        }
-        else
-        {
-            Debug.LogError("Position occupée"); 
-        }
-        return false; 
-        **/
         return false;
     }
-
-
     public override bool[,] isDanger(bool[,] danger)
     {
         return danger;
     }
-
-
     public override List<Coup> GenererCoupsPossibles(Plateau plateau)
     {
         List<Coup> coups = new List<Coup>();
